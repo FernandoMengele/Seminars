5ceeb9a68214580a2c3954eb7f90da7beb91f642
Danil <nekravtsov.d@yandex.ru>
2022-12-16 06:04:05 +0300

    Task2 added

--- Seminar001/Program.cs
+++ Seminar001/Program.cs
@@ -1,7 +1,8 @@
 ﻿// Домашняя работа по семинару 1
 
 /* Приветствую, Павел. Очень рад видеть Вас снова. 
-Надеюсь, наше сотрудничество получется таким же продуктивным, как и на семинарах по контролю версий.
+Надеюсь, наше сотрудничество получится таким же продуктивным, как и на семинарах по контролю версий.
+С уважением, Некравцов Данил.
 */
 
 // Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
@@ -15,3 +16,27 @@ int num2 = Convert.ToInt32(Console.ReadLine());
 if(num1 < num2) Console.WriteLine($"Max number: {num2}, Min number: {num1}.");
 else Console.WriteLine($"Max number: {num1}, Min number: {num2}");
 */
+
+// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if(num2 > max)  max = num2;
+else max = num1;
+if(num3 > max)
+{
+    max = num3;
+    Console.WriteLine($"Max number: {max}");
+} 
+else  Console.WriteLine($"Max number: {max}");
+
+*/
+
