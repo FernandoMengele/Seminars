09cb622e1cdb7b34028d91ddb385bd8f44011b2c
Danil <nekravtsov.d@yandex.ru>
2022-12-20 02:21:06 +0300

    seminar002 added

---
+++ Seminar002/Program.cs
@@ -0,0 +1,18 @@
+﻿/* 
+
+int CutNumber(int number)
+{
+    int sot = number / 100;
+    int ed = number % 10;
+
+    int result = sot * 10 + ed;
+    return result; // обязательно возвращаем, если не void
+}
+
+int randNumber = new Random().Next(100, 1000);
+int newNumber = CutNumber(randNumber);
+
+Console.WriteLine($"New version of a number {randNumber} is {newNumber}");
+
+*/
+
