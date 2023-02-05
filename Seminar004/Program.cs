3ecaed947d56a64b88ac75aded2cb8c0cc6c55a7
Danil <nekravtsov.d@yandex.ru>
2023-02-05 22:21:09 +0300

    Seminar004 modified

--- Seminar004/Program.cs
+++ Seminar004/Program.cs
@@ -1,72 +1,59 @@
-﻿/*
-//task 25. input: two numbers (A и B) , output: A in a natural power B. Code without using internal Pow method.
-//3, 5 -> 243 (3⁵)
-//2, 4 -> 16
-double MyPow(double num, int pow)
-{
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+/* 
+double GetNum (double num, int pow){
     double powRes = 1;
-    for (int i = 1; i <= Math.Abs(pow); i++) // if without Math.Abs() - then should check if pow>=0 before using this method
+    for (int i = 1; i <= Math.Abs(pow); i++)
         powRes *= num;
     return powRes;
 }
-Console.Write("Please input number A:  ");
-double num = Convert.ToDouble(Console.ReadLine());
-Console.Write("Please input power value B as natural number ");
+Console.Write("Input a number A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a power: ");
 int pow = Convert.ToInt32(Console.ReadLine());
-//if (pow >= 0)
-//{
-double numPow = MyPow(num, pow);
-Console.WriteLine($"The result of A ({num}) in natural B power ({Math.Abs(pow)}) is {numPow}  ");
-//}
-//else Console.WriteLine(" incorrect input, entered B is not natural ");
-*/
-/*
-//Task 27: Input: number, output: sum of its digits.
-//452 -> 11
-//82 -> 10
-//9012 -> 12
-int SumOfDigits(int num)
-{
-    int result = 0, testNum = Math.Abs(num);
-    while (testNum != 0)
-    {
-        result += testNum % 10;
-        testNum /= 10;
-    }
-    return result;
-}
-Console.Write("Please input integer number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-int digitsSum = SumOfDigits(num);
-Console.WriteLine($"The sum of the digits in entered number ({num}) is {digitsSum} ");
+Console.WriteLine(GetNum(numA, pow));
 */
 
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
 /*
-//Task 29: input: array length m and all array elements, output:  the array itself( to console as well)
-//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-//6, 1, 33 -> [6, 1, 33]
-int[] GetArrayFromUser(int length)
+int SumNumber (int num){
+    int current = 0;
+    while (num != 0){
+        num = num / 10;
+        current++;
+    }
+    return current;
+}
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNumber(num));
+*/
+
+// Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+/*
+int[] GetMassive(int length)
 {
-    int[] array = new int[length];
-    for (int i = 0; i < length; i++)
+    var array = new int[length];
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"Please input array number with index {i}: ");
+        Console.Write("Input a number: ");
         array[i] = Convert.ToInt32(Console.ReadLine());
     }
     return array;
 }
-void ArrayToConsole(int[] array)
+void GiveMassive(int[] array)
 {
-    Console.Write("Your array is [");
-    for (int i = 0; i < array.Length-1; i++)
+    Console.Write("You are array is [ ");
+    for (int i = 0; i < array.Length - 1; i++)
     {
-        Console.Write(array[i] + ", ");
+        Console.Write(array[i] + ",");
     }
     Console.Write($"{array[array.Length-1]}]");
     Console.WriteLine();
 }
-Console.Write("Please input array length ");
+Console.Write("Please input array length: ");
 int length = Convert.ToInt32(Console.ReadLine());
-int[] yourArray = GetArrayFromUser(length);
-ArrayToConsole(yourArray);
+int[] array = GetMassive(length);
+GiveMassive(array);
 */
